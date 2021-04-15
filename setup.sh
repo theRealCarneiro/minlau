84c667dbb934a35cbf8b8bf7cdc55fdf52be1adb
theRealCarneiro <gabriel.chaves.carneiro@gmail.com>
2021-04-15 16:07:14 -0300

    added man to setup

--- setup.sh
+++ setup.sh
@@ -9,12 +9,17 @@
 [ -z $ZSH_COMPLETION_DIR ] && \
 	ZSH_COMPLETION_DIR=/usr/local/share/zsh/site-functions
 
+[ -z $MAN_DIR ] && \
+	MAN_DIR=/usr/local/man/man1
+
 if [ "$1" = "install" ]; then
 	install -Dm755 src/minlau "$DEST_DIR/minlau"
 	install -Dm644 LICENSE "$LICENSE_DIR/LICENSE"
 	install -Dm644 completion/_minlau "$ZSH_COMPLETION_DIR/_minlau"
+	install -Dm644 minlau.1 "$MAN_DIR/minlau.1"
 elif [ "$1" = "uninstall" ]; then
 	rm "$DEST_DIR/minlau"
 	rm -r "$LICENSE_DIR"
 	rm "$ZSH_COMPLETION_DIR/_minlau"
+	rm "$MAN_DIR/minlau.1"
 fi
