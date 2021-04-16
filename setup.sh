9ae00d0051de780b7428d239e5bcda89bc78d3c1
theRealCarneiro <gabriel.chaves.carneiro@gmail.com>
2021-04-16 09:43:51 -0300

    improved setup.sh variables, now installation is better

--- setup.sh
+++ setup.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
 
-[ -z $DEST_DIR ] && \
-	DEST_DIR=/usr/local/bin
+[ -z $PREFIX ] && \
+	PREFIX=/usr/local
+
+[ -z $BIN_DIR ] && \
+	BIN_DIR=$DESTDIR$PREFIX/bin
 
 [ -z $LICENSE_DIR ] && \
-	LICENSE_DIR=/usr/local/share/licenses/minlau
+	LICENSE_DIR=$DESTDIR$PREFIX/share/licenses/minlau
 
 [ -z $ZSH_COMPLETION_DIR ] && \
-	ZSH_COMPLETION_DIR=/usr/local/share/zsh/site-functions
+	ZSH_COMPLETION_DIR=$DESTDIR$PREFIX/share/zsh/site-functions
 
 [ -z $MAN_DIR ] && \
-	MAN_DIR=/usr/local/man/man1
+	MAN_DIR=$DESTDIR$PREFIX/man/man1
 
 if [ "$1" = "install" ]; then
-	install -Dm755 src/minlau "$DEST_DIR/minlau"
+	install -Dm755 src/minlau "$BIN_DIR/minlau"
 	install -Dm644 LICENSE "$LICENSE_DIR/LICENSE"
 	install -Dm644 completion/_minlau "$ZSH_COMPLETION_DIR/_minlau"
 	install -Dm644 minlau.1 "$MAN_DIR/minlau.1"
 elif [ "$1" = "uninstall" ]; then
-	rm "$DEST_DIR/minlau"
+	rm "$BIN_DIR/minlau"
 	rm -r "$LICENSE_DIR"
 	rm "$ZSH_COMPLETION_DIR/_minlau"
 	rm "$MAN_DIR/minlau.1"
